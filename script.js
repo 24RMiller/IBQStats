aa43bfe279ce6f11d12b5e84e56334446e4afd86
Ryan Miller <109994547+24RMiller@users.noreply.github.com>
2026-06-11 13:52:40 -0400

    Update fetch URL to use CSV output format

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const START_ROW = 73;
 const END_ROW = 86;
 
 async function loadStats() {
-  const res = await fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vQxrVRVkdMhenEIf_MA6dfUbDmMh_RIV5sLtaELe4dJHqvfDFO_FX-sSDEniujhf2tsD3y731Y4KDdt/pubhtml");
+  const res = await fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vQxrVRVkdMhenEIf_MA6dfUbDmMh_RIV5sLtaELe4dJHqvfDFO_FX-sSDEniujhf2tsD3y731Y4KDdt/pub?output=csv");
   const text = await res.text();
 
   const rows = text.trim().split("\n").map(r => r.split(","));
