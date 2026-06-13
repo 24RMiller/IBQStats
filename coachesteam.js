fe957a72d9e82afcc4e9dc683a2aeebfd0b9c4cf
Ryan Miller <109994547+24RMiller@users.noreply.github.com>
2026-06-13 17:40:15 -0400

    Fix typo in SHEET_URL variable name

--- coachesteam.js
+++ coachesteam.js
@@ -1,11 +1,11 @@
 const START_ROWT = 72;
 const END_ROWT = 86;
 
-const SHEET_URL =
+const SHEET_URLT =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQxrVRVkdMhenEIf_MA6dfUbDmMh_RIV5sLtaELe4dJHqvfDFO_FX-sSDEniujhf2tsD3y731Y4KDdt/pub?output=csv";
 
 async function loadStats() {
-  const res = await fetch(SHEET_URL);
+  const res = await fetch(SHEET_URLT);
   const text = await res.text();
 
   const rows = text.trim().split("\n").map(r => r.split(","));
